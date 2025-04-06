e127be6a041a3c94f08259c53768f543b11d25ba
Mikko Keinänen <mikko.keinanen@gmail.com>
2025-04-06 11:24:42 +0300

    move integration test to tests folder

--- file_export/src/lib.rs
+++ file_export/src/lib.rs
@@ -1,9 +1,4 @@
-use std::{
-    collections::HashMap,
-    fs::File,
-    io::{Read, Seek, SeekFrom},
-    path::Path,
-};
+use std::{collections::HashMap, fs::File, io::Read, path::Path};
 
 use sha1::{Digest, Sha1};
 
@@ -25,104 +20,51 @@ pub fn export_files(
     _export_type: ExportType,
 ) -> Result<(), Box<dyn std::error::Error>> {
     for (archive_file_name, output_file_name) in output_file_name_mapping {
-        println!(
-            "Exporting file: {} to {}",
-            archive_file_name, output_file_name
-        );
         // souce files are in zstd format
         let file_path = file_path.join(&archive_file_name).with_extension("zst");
-        println!(
-            "File path: {}",
-            file_path.to_str().unwrap_or("Invalid path")
-        );
-        let file = File::open(&file_path)?;
-        // create decoder to decompress the zstd file
-        let mut zstd_reader = zstd::Decoder::new(file)?;
         let output_file_path = output_dir.join(&output_file_name);
-        println!(
-            "Output file path: {}",
-            output_file_path.to_str().unwrap_or("Invalid path")
-        );
-        if let Some(parent) = output_file_path.parent() {
-            println!(
-                "Creating parent directory: {}",
-                parent.to_str().unwrap_or("Invalid path")
-            );
-            std::fs::create_dir_all(parent)?;
-        }
-        let mut output_file = File::create(&output_file_path)?;
-
-        // copy the decompressed data to the output file
-        std::io::copy(&mut zstd_reader, &mut output_file)?;
-
-        // create hasher and buffer for calculating checksum
-        let mut hasher = Sha1::new();
-        let mut buffer = Vec::new();
-        println!("Calculating checksum for file: {}", archive_file_name);
-        let mut output_file = File::open(&output_file_path)?;
-        output_file.seek(SeekFrom::Start(0))?;
-        output_file.read_to_end(&mut buffer)?;
-        hasher.update(&buffer);
-        let calculated_checksum = hasher.finalize();
-        if format!("{:x}", calculated_checksum)
-            != *filename_checksum_mapping.get(&archive_file_name).unwrap()
-        {
+        decompress_zstd_file(&file_path, &output_file_path)?;
+        if let Err(e) = check_file_checksum(
+            &output_file_path,
+            filename_checksum_mapping.get(&archive_file_name).unwrap(),
+        ) {
             return Err(format!(
-                "Checksum verification failed for file: {}",
-                archive_file_name
+                "Checksum verification failed for file: {}. Error: {}",
+                archive_file_name, e
             )
             .into());
         }
     }
-
     Ok(())
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use std::collections::HashMap;
-    use std::fs::{self, File};
-    use std::io::Write;
-    use tempfile::tempdir;
-
-    #[test]
-    fn test_export_files() {
-        // Create a temporary directory for input and output
-        let temp_dir = tempdir().unwrap();
-        let input_dir = temp_dir.path().join("input");
-        let output_dir = temp_dir.path().join("output");
-        fs::create_dir_all(&input_dir).unwrap();
-        fs::create_dir_all(&output_dir).unwrap();
-
-        // Create a sample compressed file
-        let file_name = "test_file";
-        let test_file_content = "Hello, world!";
-        let test_file_content_sha1 = "943a702d06f34599aee1f8da8ef9f7296031d699";
-        let compressed_file_path = input_dir.join(format!("{}.zst", file_name));
-        let mut encoder =
-            zstd::Encoder::new(File::create(&compressed_file_path).unwrap(), 0).unwrap();
-        write!(encoder, "{}", test_file_content).unwrap();
-        encoder.finish().unwrap();
-
-        // Prepare file mappings
-        let mut output_file_name_mapping = HashMap::new();
-        output_file_name_mapping.insert(file_name.to_string(), "output_file".to_string());
-        let mut filename_checksum_mapping = HashMap::new();
-        filename_checksum_mapping.insert(file_name.to_string(), test_file_content_sha1.to_string());
-
-        export_files(
-            &input_dir,
-            &output_dir,
-            output_file_name_mapping,
-            filename_checksum_mapping,
-            ExportType::IndividualFilesWithoutCompression,
-        )
-        .unwrap();
-
-        let output_file_path = output_dir.join("output_file");
-        assert!(output_file_path.exists());
-        let content = fs::read_to_string(output_file_path).unwrap();
-        assert_eq!(content, "Hello, world!");
+fn decompress_zstd_file(
+    input_path: &Path,
+    output_path: &Path,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let file = File::open(input_path)?;
+    let mut zstd_reader = zstd::Decoder::new(file)?;
+    if let Some(parent) = output_path.parent() {
+        println!(
+            "Creating parent directory: {}",
+            parent.to_str().unwrap_or("Invalid path")
+        );
+        std::fs::create_dir_all(parent)?;
     }
+    let mut output_file = File::create(output_path)?;
+    std::io::copy(&mut zstd_reader, &mut output_file)?;
+    Ok(())
+}
+
+fn check_file_checksum(
+    file_path: &Path,
+    expected_checksum: &str,
+) -> Result<bool, Box<dyn std::error::Error>> {
+    let mut hasher = Sha1::new();
+    let mut file = File::open(file_path)?;
+    let mut buffer = Vec::new();
+    file.read_to_end(&mut buffer)?;
+    hasher.update(&buffer);
+    let calculated_checksum = hasher.finalize();
+    Ok(format!("{:x}", calculated_checksum) == expected_checksum)
 }
