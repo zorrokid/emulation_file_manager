39c4bfe956c6d7a02d18388a29e5850b9484ea03
Mikko Keinänen <mikko.keinanen@gmail.com>
2025-04-08 22:51:14 +0300

    instantiate repository in main

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,6 +1,8 @@
-use std::{collections::HashMap, path::Path};
+use std::{collections::HashMap, path::Path, sync::Arc};
 
+use async_std::task;
 use clap::Parser;
+use database::{get_db_pool, repository::file_info_repository::FileInfoRepository};
 use file_export::{export_files, export_files_zipped};
 use file_import::{read_zip_file, CompressionMethod};
 
@@ -17,39 +19,46 @@ struct Cli {
     compression_method: CompressionMethod,
 }
 
-fn main() {
-    let args = Cli::parse();
-    match read_zip_file(
-        args.input_file.as_str(),
-        args.output_directory.as_str(),
-        args.compression_method,
-    ) {
-        Ok(hash_map) => {
-            // let's try exporting the files...
-            let input_path = Path::new(&args.output_directory);
-            let output_path = Path::new(&args.output_directory).join("export");
-            let output_filename_mapping = hash_map
-                .iter()
-                .map(|(k, v)| (k.clone(), format!("{}-{}-exported", k.clone(), v.clone())))
-                .collect::<HashMap<_, _>>();
-            export_files(
-                input_path,
-                &output_path,
-                output_filename_mapping.clone(),
-                hash_map.clone(),
-            )
-            .expect("Failed to export files");
-            export_files_zipped(
-                input_path,
-                &output_path,
-                output_filename_mapping,
-                hash_map.clone(),
-                "exported_files.zip".to_string(),
-            )
-            .expect("Failed to export files");
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    task::block_on(async {
+        let args = Cli::parse();
+        let db_pool = get_db_pool().await.unwrap();
+
+        let file_info_repository = FileInfoRepository::new(Arc::clone(&db_pool));
+
+        match read_zip_file(
+            args.input_file.as_str(),
+            args.output_directory.as_str(),
+            args.compression_method,
+        ) {
+            Ok(hash_map) => {
+                // let's try exporting the files...
+                let input_path = Path::new(&args.output_directory);
+                let output_path = Path::new(&args.output_directory).join("export");
+                let output_filename_mapping = hash_map
+                    .iter()
+                    .map(|(k, v)| (k.clone(), format!("{}-{}-exported", k.clone(), v.clone())))
+                    .collect::<HashMap<_, _>>();
+                export_files(
+                    input_path,
+                    &output_path,
+                    output_filename_mapping.clone(),
+                    hash_map.clone(),
+                )
+                .expect("Failed to export files");
+                export_files_zipped(
+                    input_path,
+                    &output_path,
+                    output_filename_mapping,
+                    hash_map.clone(),
+                    "exported_files.zip".to_string(),
+                )
+                .expect("Failed to export files");
+            }
+            Err(e) => {
+                eprintln!("Error reading zip file: {}", e);
+            }
         }
-        Err(e) => {
-            eprintln!("Error reading zip file: {}", e);
-        }
-    }
+        Ok(())
+    })
 }
