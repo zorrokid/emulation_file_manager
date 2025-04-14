57a8a8419058d3cc0dbc12f6f00d9df7fb56920c
Mikko Keinänen <mikko.keinanen@gmail.com>
2025-04-14 19:55:10 +0300

    test: is system is in use test

--- database/src/repository/system_repository.rs
+++ database/src/repository/system_repository.rs
@@ -101,4 +101,70 @@ mod tests {
             .unwrap();
         result.last_insert_rowid()
     }
+
+    async fn insert_test_release(pool: &Pool<Sqlite>) -> i64 {
+        let result = query!("INSERT INTO release (name) VALUES(?)", "Some release")
+            .execute(pool)
+            .await
+            .unwrap();
+        result.last_insert_rowid()
+    }
+
+    async fn insert_test_emulator(pool: &Pool<Sqlite>) -> i64 {
+        let result = query!(
+            "INSERT INTO emulator (name, executable, extract_files) VALUES(?,?,?)",
+            "Test Emulator",
+            "temu",
+            false
+        )
+        .execute(pool)
+        .await
+        .unwrap();
+        result.last_insert_rowid()
+    }
+
+    #[async_std::test]
+    async fn test_is_system_in_use() {
+        let pool = Arc::new(setup_test_db().await);
+        let system_id = insert_test_system(&pool.clone(), TEST_SYSTEM_NAME).await;
+        let release_id = insert_test_release(&pool.clone()).await;
+        let emulator_id = insert_test_emulator(&pool.clone()).await;
+
+        let repo = SystemRepository { pool: pool.clone() };
+
+        let result = repo.is_system_in_use(system_id).await.unwrap();
+        assert!(!result);
+
+        query!(
+            "INSERT INTO release_system (release_id, system_id) VALUES (?, ?)",
+            release_id,
+            system_id
+        )
+        .execute(&*pool.clone())
+        .await
+        .unwrap();
+
+        let result = repo.is_system_in_use(system_id).await.unwrap();
+        assert!(result);
+
+        query!("DELETE FROM release_system")
+            .execute(&*pool.clone())
+            .await
+            .unwrap();
+
+        let result = repo.is_system_in_use(system_id).await.unwrap();
+        assert!(!result);
+
+        query!(
+            "INSERT INTO emulator_system (emulator_id, system_id) VALUES (?, ?)",
+            emulator_id,
+            system_id
+        )
+        .execute(&*pool.clone())
+        .await
+        .unwrap();
+
+        let result = repo.is_system_in_use(system_id).await.unwrap();
+        assert!(result);
+    }
 }
