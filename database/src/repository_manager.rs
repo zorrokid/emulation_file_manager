d26128644806b34e6be92cfe578e2eb80bebb17e
Mikko Keinänen <mikko.keinanen@gmail.com>
2025-04-15 21:35:49 +0300

    feat: add new repositories to repository manager

--- database/src/repository_manager.rs
+++ database/src/repository_manager.rs
@@ -4,13 +4,19 @@ use sqlx::{Pool, Sqlite};
 
 use crate::repository::{
     emulator_repository::EmulatorRepository, file_info_repository::FileInfoRepository,
-    file_set_repository::FileSetRepository, system_repository::SystemRepository,
+    file_set_repository::FileSetRepository, franchise_repository::FranchiseRepository,
+    release_repository::ReleaseRepository, software_title_repository::SoftwareTitleRepository,
+    system_repository::SystemRepository,
 };
+
 pub struct RepositoryManager {
     file_info_repository: FileInfoRepository,
     file_set_repository: FileSetRepository,
     emulator_repository: EmulatorRepository,
     system_repository: SystemRepository,
+    franchise_repository: FranchiseRepository,
+    release_repository: ReleaseRepository,
+    software_title_repository: SoftwareTitleRepository,
 }
 
 impl RepositoryManager {
@@ -19,12 +25,18 @@ impl RepositoryManager {
         let file_set_repository = FileSetRepository::new(pool.clone());
         let emulator_repository = EmulatorRepository::new(pool.clone());
         let system_repository = SystemRepository::new(pool.clone());
+        let franchise_repository = FranchiseRepository::new(pool.clone());
+        let release_repository = ReleaseRepository::new(pool.clone());
+        let software_title_repository = SoftwareTitleRepository::new(pool.clone());
 
         Self {
             file_info_repository,
             file_set_repository,
             emulator_repository,
             system_repository,
+            franchise_repository,
+            release_repository,
+            software_title_repository,
         }
     }
 
@@ -39,7 +51,20 @@ impl RepositoryManager {
     pub fn get_emulator_repository(&self) -> &EmulatorRepository {
         &self.emulator_repository
     }
+
     pub fn get_system_repository(&self) -> &SystemRepository {
         &self.system_repository
+    }
+
+    pub fn get_franchise_repository(&self) -> &FranchiseRepository {
+        &self.franchise_repository
+    }
+
+    pub fn get_release_repository(&self) -> &ReleaseRepository {
+        &self.release_repository
+    }
+
+    pub fn get_software_title_repository(&self) -> &SoftwareTitleRepository {
+        &self.software_title_repository
     }
 }
