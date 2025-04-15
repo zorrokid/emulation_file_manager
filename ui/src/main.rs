92dfd3b12a6e8d456f72257a60c1100e6857b540
Mikko Keinänen <mikko.keinanen@gmail.com>
2025-04-15 22:02:58 +0300

    feat: initial ui

--- ui/src/main.rs
+++ ui/src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use iced::{widget::text, Task};
+
+fn main() -> iced::Result {
+    iced::application(Ui::title, Ui::update, Ui::view).run_with(Ui::new)
+}
+
+struct Ui {}
+
+#[derive(Debug, Clone)]
+enum Message {}
+
+impl Ui {
+    fn new() -> (Self, Task<Message>) {
+        (Self {}, iced::Task::none())
+    }
+
+    fn title(&self) -> String {
+        "My Application".to_string()
+    }
+
+    fn update(&mut self, message: Message) -> Task<Message> {
+        iced::Task::none()
+    }
+
+    fn view(&self) -> iced::Element<Message> {
+        text!("TODO").into()
+    }
 }
