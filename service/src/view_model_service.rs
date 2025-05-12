ec4378547e76e94300453979202058dadb94b727
Mikko Keinänen <mikko.keinanen@gmail.com>
2025-05-12 21:50:57 +0300

    chore: fix test

--- service/src/view_model_service.rs
+++ service/src/view_model_service.rs
@@ -148,22 +148,21 @@ mod tests {
         let pool = Arc::new(pool);
         let repository_manager = Arc::new(RepositoryManager::new(pool.clone()));
         let view_model_service = ViewModelService::new(repository_manager.clone());
-
-        let emulator_id = repository_manager
-            .get_emulator_repository()
-            .add_emulator("Test Emulator".to_string(), "temu".to_string(), false)
-            .await
-            .unwrap();
-
         let system_id = repository_manager
             .get_system_repository()
             .add_system("Test System".to_string())
             .await
             .unwrap();
+        let emulator_systems = vec![(system_id, "args".to_string())];
 
-        repository_manager
+        let emulator_id = repository_manager
             .get_emulator_repository()
-            .add_emulator_system(emulator_id, system_id, "args".to_string())
+            .add_emulator_with_systems(
+                "Test Emulator".to_string(),
+                "temu".to_string(),
+                false,
+                emulator_systems,
+            )
             .await
             .unwrap();
 
