1240e4336edee685d0c677c25c591825cdd0593f
Mikko Keinänen <mikko.keinanen@gmail.com>
2025-05-18 13:25:56 +0300

    refactor: move widgets from release tab to widget

--- ui/src/widgets/release_widget.rs
+++ ui/src/widgets/release_widget.rs
@@ -1,19 +1,185 @@
-use iced::{widget::text, Element, Task};
+use std::sync::Arc;
 
-pub struct ReleaseWidget {}
+use database::{database_error::Error, repository_manager::RepositoryManager};
+use iced::{
+    widget::{button, column},
+    Element, Task,
+};
+use service::view_model_service::ViewModelService;
 
-pub enum Message {}
+use super::{
+    file_select_widget,
+    files_widget::{self, FilesWidget},
+    software_title_select_widget,
+    software_titles_widget::{self, SoftwareTitlesWidget},
+    system_select_widget,
+    systems_widget::{self, SystemsWidget},
+};
+
+pub struct ReleaseWidget {
+    systems_widget: SystemsWidget,
+    selected_system_ids: Vec<i64>,
+    software_titles_widget: SoftwareTitlesWidget,
+    selected_software_title_ids: Vec<i64>,
+    files_widget: FilesWidget,
+    selected_file_ids: Vec<i64>,
+    repositories: Arc<RepositoryManager>,
+}
+
+#[derive(Debug, Clone)]
+pub enum Message {
+    Systems(systems_widget::Message),
+    SoftwareTitles(software_titles_widget::Message),
+    Files(files_widget::Message),
+    Submit,
+    ReleaseSubmitted(Result<i64, Error>),
+}
 
 impl ReleaseWidget {
-    pub fn new() -> Self {
-        Self {}
+    pub fn new(
+        repositories: Arc<RepositoryManager>,
+        view_model_service: Arc<ViewModelService>,
+    ) -> (Self, Task<Message>) {
+        let (systems_widget, systems_task) =
+            SystemsWidget::new(Arc::clone(&repositories), Arc::clone(&view_model_service));
+        let (software_titles_widget, software_titles_task) =
+            SoftwareTitlesWidget::new(Arc::clone(&repositories), Arc::clone(&view_model_service));
+
+        let (files_widget, files_task) =
+            FilesWidget::new(Arc::clone(&repositories), Arc::clone(&view_model_service));
+
+        let combined_task = Task::batch(vec![
+            systems_task.map(Message::Systems),
+            software_titles_task.map(Message::SoftwareTitles),
+            files_task.map(Message::Files),
+        ]);
+
+        (
+            Self {
+                systems_widget,
+                selected_system_ids: vec![],
+                software_titles_widget,
+                selected_software_title_ids: vec![],
+                files_widget,
+                selected_file_ids: vec![],
+                repositories,
+            },
+            combined_task,
+        )
     }
 
     pub fn update(&mut self, message: Message) -> Task<Message> {
-        match message {}
+        match message {
+            Message::Systems(message) => {
+                match &message {
+                    systems_widget::Message::SystemSelect(
+                        system_select_widget::Message::SystemSelected(system),
+                    ) => {
+                        println!("AddReleaseTab: System selected: {:?}", system);
+                        self.selected_system_ids.push(system.id);
+                    }
+                    systems_widget::Message::RemoveSystem(system_id) => {
+                        println!("AddReleaseTab: System removed: {:?}", system_id);
+                        self.selected_system_ids.retain(|&id| id != *system_id);
+                    }
+                    _ => {}
+                }
+                self.systems_widget.update(message).map(Message::Systems)
+            }
+            Message::SoftwareTitles(message) => {
+                match &message {
+                    software_titles_widget::Message::SoftwareTitleSelect(
+                        software_title_select_widget::Message::SoftwareTitleSelected(
+                            software_title,
+                        ),
+                    ) => {
+                        println!(
+                            "AddReleaseTab: Software title selected: {:?}",
+                            software_title
+                        );
+                        self.selected_software_title_ids.push(software_title.id);
+                    }
+                    software_titles_widget::Message::RemoveSoftwareTitle(software_title_id) => {
+                        println!(
+                            "AddReleaseTab: Software title removed: {:?}",
+                            software_title_id
+                        );
+                        self.selected_software_title_ids
+                            .retain(|&id| id != *software_title_id);
+                    }
+                    _ => {}
+                }
+
+                self.software_titles_widget
+                    .update(message)
+                    .map(Message::SoftwareTitles)
+            }
+            Message::Files(message) => {
+                match &message {
+                    files_widget::Message::FileSelect(
+                        file_select_widget::Message::FileSelected(file),
+                    ) => {
+                        println!("AddReleaseTab: File selected: {:?}", file);
+                        self.selected_file_ids.push(file.id);
+                    }
+                    files_widget::Message::RemoveFile(file_id) => {
+                        println!("AddReleaseTab: File removed: {:?}", file_id);
+                        self.selected_file_ids.retain(|&id| id != *file_id);
+                    }
+                    _ => {}
+                }
+
+                self.files_widget.update(message).map(Message::Files)
+            }
+            Message::Submit => {
+                println!("AddReleaseTab: Submit button pressed");
+                let repositories = Arc::clone(&self.repositories);
+                let software_title_ids = self.selected_software_title_ids.clone();
+                let file_set_ids = self.selected_file_ids.clone();
+                let system_ids = self.selected_system_ids.clone();
+                Task::perform(
+                    async move {
+                        repositories
+                            .get_release_repository()
+                            .add_release_full(
+                                "".to_string(),
+                                software_title_ids,
+                                file_set_ids,
+                                system_ids,
+                            )
+                            .await
+                    },
+                    Message::ReleaseSubmitted,
+                )
+            }
+            Message::ReleaseSubmitted(result) => match result {
+                Ok(id) => {
+                    // TODO
+                    print!("Release {} submitted", id);
+                    Task::none()
+                }
+                Err(err) => {
+                    eprintln!("Error submitting file: {}", err);
+                    Task::none()
+                }
+            },
+        }
     }
 
     pub fn view(&self) -> Element<Message> {
-        text!("Release Widget").into()
+        let systems_view = self.systems_widget.view().map(Message::Systems);
+        let software_titles_view = self
+            .software_titles_widget
+            .view()
+            .map(Message::SoftwareTitles);
+        let files_view = self.files_widget.view().map(Message::Files);
+        let submit_button = button("Submit").on_press(Message::Submit);
+        column![
+            software_titles_view,
+            systems_view,
+            files_view,
+            submit_button
+        ]
+        .into()
     }
 }
