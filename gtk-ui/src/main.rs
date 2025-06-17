572057ba0b5dca266e6514ba705029092eb01b2a
Mikko Keinänen <mikko.keinanen@gmail.com>
2025-06-17 22:42:45 +0300

    feat: reference management

--- gtk-ui/src/main.rs
+++ gtk-ui/src/main.rs
@@ -29,12 +29,28 @@ fn build_ui(app: &Application) {
         .margin_end(50)
         .build();
 
+    // Rc is a reference counted pointer, which allows multiple ownership of the same value.
+    // Cell is a type that allows for interior mutability, meaning that it can be mutated even when
+    // the value is behind an immutable reference.
     let number = Rc::new(Cell::new(0));
+
+    // The clone! macro is used to capture references to the widgets and number for use inside the closures, ensuring proper memory management with #[weak] to avoid reference cycles.
+    //
+    // A strong reference keeps the referenced value from being deallocated. If this
+    // chain leads to a circle, none of the values in this cycle ever get deallocated.
+    //
+    // A weak reference does not keep the value from being deallocated. If the value is
+    // deallocated, the weak reference will return None when accessed.
+    //
+    // Every time the button is clicked, glib::clone tries to upgrade the weak reference.
+    // If we now for example click on one button and the other button is not there anymore, the callback will be skipped.
+    // Per default, it immediately returns from the closure with () as return value.
+    // In case the closure expects a different return value @default-return can be specified.
 
     button_increase.connect_clicked(clone!(
         #[weak]
         number,
-        #[strong]
+        #[weak]
         button_decrease,
         move |_| {
             number.set(number.get() + 1);
@@ -42,7 +58,8 @@ fn build_ui(app: &Application) {
         }
     ));
     button_decrease.connect_clicked(clone!(
-        #[strong]
+        // No weak reference for number here, as we want to keep it alive for both buttons.
+        #[weak]
         button_increase,
         move |_| {
             number.set(number.get() - 1);
@@ -53,6 +70,7 @@ fn build_ui(app: &Application) {
     let gtk_box = gtk::Box::builder()
         .orientation(gtk::Orientation::Vertical)
         .build();
+    // gtk_box keeps strong references to its children (keeps those buttons alive),
     gtk_box.append(&button_increase);
     gtk_box.append(&button_decrease);
 
