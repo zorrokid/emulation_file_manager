d17eb66227696f06a9488dec65e4cf1f4f5fc445
Mikko Keinänen <mikko.keinanen@gmail.com>
2025-06-17 22:18:36 +0300

    feat: using glib clone

--- gtk-ui/src/main.rs
+++ gtk-ui/src/main.rs
@@ -1,8 +1,8 @@
-use std::cell::Cell;
-use std::rc::Rc;
-
+use glib::clone;
 use gtk::prelude::*;
 use gtk::{glib, Application, Button};
+use std::cell::Cell;
+use std::rc::Rc;
 
 const APP_ID: &str = "org.zorrokid.scm";
 
@@ -13,8 +13,16 @@ fn main() -> glib::ExitCode {
 }
 
 fn build_ui(app: &Application) {
-    let button = Button::builder()
-        .label("Click Me")
+    let button_increase = Button::builder()
+        .label("Increase")
+        .margin_top(50)
+        .margin_bottom(50)
+        .margin_start(50)
+        .margin_end(50)
+        .build();
+
+    let button_decrease = Button::builder()
+        .label("Decrease")
         .margin_top(50)
         .margin_bottom(50)
         .margin_start(50)
@@ -23,18 +31,35 @@ fn build_ui(app: &Application) {
 
     let number = Rc::new(Cell::new(0));
 
-    let number_clone = Rc::clone(&number);
-    button.connect_clicked(move |_| {
-        number_clone.set(number_clone.get() + 1);
-    });
-    button.connect_clicked(move |_| {
-        number.set(number.get() - 1);
-    });
+    button_increase.connect_clicked(clone!(
+        #[weak]
+        number,
+        #[strong]
+        button_decrease,
+        move |_| {
+            number.set(number.get() + 1);
+            button_decrease.set_label(&format!("Decrease: {}", number.get()));
+        }
+    ));
+    button_decrease.connect_clicked(clone!(
+        #[strong]
+        button_increase,
+        move |_| {
+            number.set(number.get() - 1);
+            button_increase.set_label(&format!("Increase: {}", number.get()));
+        }
+    ));
+
+    let gtk_box = gtk::Box::builder()
+        .orientation(gtk::Orientation::Vertical)
+        .build();
+    gtk_box.append(&button_increase);
+    gtk_box.append(&button_decrease);
 
     let window = gtk::ApplicationWindow::builder()
         .application(app)
         .title("SCM")
-        .child(&button)
+        .child(&gtk_box)
         .build();
 
     window.present();
