b2d2a11c8fbe908831e417a21c56c4652029227b
Mikko Keinänen <mikko.keinanen@gmail.com>
2025-06-21 12:31:53 +0300

    feat: custom button properties and signals

--- gtk-ui/src/custom_button/imp.rs
+++ gtk-ui/src/custom_button/imp.rs
@@ -1,12 +1,18 @@
 use std::cell::Cell;
+use std::sync::OnceLock;
 
-use gtk::glib;
+use glib::types::StaticType;
+use gtk::glib::object::ObjectExt;
+use gtk::glib::subclass::Signal;
+use gtk::glib::{self, Properties};
 use gtk::prelude::ButtonExt;
 use gtk::subclass::prelude::*;
 
 // object holding the state
-#[derive(Default)]
+#[derive(Properties, Default)]
+#[properties(wrapper_type = super::CustomButton)]
 pub struct CustomButton {
+    #[property(get, set)]
     number: Cell<i32>,
 }
 
@@ -21,22 +27,47 @@ impl ObjectSubclass for CustomButton {
     type ParentType = gtk::Button;
 }
 
+#[glib::derived_properties]
 // trait shared by all GObjects
 impl ObjectImpl for CustomButton {
     // This function is called when the object is constructed.
     fn constructed(&self) {
         self.parent_constructed();
         self.obj().set_label(&self.number.get().to_string());
+
+        // Bind the "label" property to the number property
+        let obj = self.obj();
+        obj.bind_property("number", obj.as_ref(), "label")
+            .sync_create()
+            .build();
+    }
+
+    fn signals() -> &'static [Signal] {
+        static SIGNALS: OnceLock<Vec<Signal>> = OnceLock::new();
+        SIGNALS.get_or_init(|| {
+            vec![Signal::builder("max-number-reached")
+                .param_types([i32::static_type()])
+                .build()]
+        })
     }
 }
 
 // trait shared by all widgets
 impl WidgetImpl for CustomButton {}
 
+static MAX_NUMBER: i32 = 6;
+
 // trait shared by all buttons
 impl ButtonImpl for CustomButton {
     fn clicked(&self) {
-        self.number.set(self.number.get() + 1);
-        self.obj().set_label(&self.number.get().to_string());
+        let incremented_number = self.obj().number() + 1;
+        let obj = self.obj();
+        if incremented_number > MAX_NUMBER {
+            // Emit the custom signal if the max number is reached
+            obj.emit_by_name::<()>("max-number-reached", &[&incremented_number]);
+            obj.set_number(0);
+        } else {
+            obj.set_number(incremented_number);
+        }
     }
 }
