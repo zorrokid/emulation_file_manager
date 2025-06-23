0a97395297a54c4aaf7322f9713251e7265096f7
Mikko Keinänen <mikko.keinanen@gmail.com>
2025-06-23 22:19:14 +0300

    feat: scalable list

--- gtk-example-list-widgets/src/main.rs
+++ gtk-example-list-widgets/src/main.rs
@@ -1,5 +1,11 @@
-use gtk::prelude::*;
-use gtk::{Application, ApplicationWindow, Label, ListBox, PolicyType, ScrolledWindow, glib};
+mod integer_object;
+use gtk::{
+    Application, ApplicationWindow, Label, ListBox, ListItem, ListView, PolicyType, ScrolledWindow,
+    SignalListItemFactory, SingleSelection, glib,
+};
+use gtk::{gio, prelude::*};
+
+use integer_object::IntegerObject;
 
 const APP_ID: &str = "org.gtk_rs.ListWidgets1";
 
@@ -15,16 +21,49 @@ fn main() -> glib::ExitCode {
 }
 
 fn build_ui(app: &Application) {
+    let vector: Vec<IntegerObject> = (1..=100_000).map(IntegerObject::new).collect();
+
+    let model = gio::ListStore::new::<IntegerObject>();
+    model.extend_from_slice(&vector);
     let list_box = ListBox::new();
     for number in 1..=100 {
         let label = Label::new(Some(&number.to_string()));
         list_box.append(&label);
     }
 
+    let factory = SignalListItemFactory::new();
+    factory.connect_setup(move |_, item| {
+        let label = Label::new(None);
+        item.downcast_ref::<ListItem>()
+            .expect("Failed to downcast to ListItem")
+            .set_child(Some(&label));
+    });
+
+    factory.connect_bind(move |_, item| {
+        let integer_object = item
+            .downcast_ref::<ListItem>()
+            .expect("Failed to downcast to ListItem")
+            .item()
+            .and_downcast::<IntegerObject>()
+            .expect("Failed to downcast to IntegerObject");
+
+        let label = item
+            .downcast_ref::<ListItem>()
+            .expect("Failed to downcast to ListItem")
+            .child()
+            .and_downcast::<Label>()
+            .expect("Failed to downcast to Label");
+
+        label.set_label(&integer_object.number().to_string());
+    });
+
+    let selection_model = SingleSelection::new(Some(model));
+    let list_view = ListView::new(Some(selection_model), Some(factory));
+
     let scrolled_window = ScrolledWindow::builder()
         .hscrollbar_policy(PolicyType::Never)
         .min_content_width(360)
-        .child(&list_box)
+        .child(&list_view)
         .build();
 
     let window = ApplicationWindow::builder()
