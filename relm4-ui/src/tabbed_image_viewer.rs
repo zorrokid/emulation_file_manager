f8145d73c8049ea14949372dae150613173dc640
Mikko Keinänen <mikko.keinanen@gmail.com>
2025-08-17 00:11:02 +0300

    feat(relm4-ui): build tabs dynamically

--- relm4-ui/src/tabbed_image_viewer.rs
+++ relm4-ui/src/tabbed_image_viewer.rs
@@ -1,6 +1,6 @@
 use std::sync::Arc;
 
-use core_types::FileType;
+use core_types::{FileType, IMAGE_FILE_TYPES};
 use relm4::{
     Component, ComponentController, ComponentParts, ComponentSender, Controller,
     gtk::{self, prelude::*},
@@ -62,65 +62,48 @@ impl Component for TabbedImageViewer {
     fn update(&mut self, msg: Self::Input, sender: ComponentSender<Self>, root: &Self::Root) {
         match msg {
             TabbedImageViewerMsg::SetFileSets { file_sets } => {
-                println!("Setting file sets in TabbedImageViewer: {:?}", file_sets);
-
                 self.viewers = Vec::new();
-                for page_number in self.page_numbers.iter() {
+                for page_number in self.page_numbers.iter().rev() {
                     root.remove_page(Some(*page_number));
                 }
+                self.page_numbers.clear();
 
-                // NOTE: currentely only the first file set of each type is used
-                if let Some(file_set) = file_sets
-                    .iter()
-                    .find(|fs| fs.file_type == FileType::CoverScan.into())
-                {
-                    let image_viewer_init = ImageViewerInit {
-                        settings: Arc::clone(&self.settings),
-                        file_set: Some(file_set.clone()),
-                    };
+                for file_type in IMAGE_FILE_TYPES {
+                    // NOTE: currentely only the first file set of each type is used
+                    if let Some(file_set) = file_sets
+                        .iter()
+                        .find(|fs| fs.file_type == (*file_type).into())
+                    {
+                        if file_set.files.is_empty() {
+                            continue; // Skip empty file sets
+                        }
+                        let image_viewer_init = ImageViewerInit {
+                            settings: Arc::clone(&self.settings),
+                            file_set: Some(file_set.clone()),
+                        };
 
-                    let box_scan_image_viewer =
-                        ImageViewer::builder().launch(image_viewer_init).detach();
+                        let box_scan_image_viewer =
+                            ImageViewer::builder().launch(image_viewer_init).detach();
 
-                    let box_scans_page = gtk::Box::builder()
-                        .orientation(gtk::Orientation::Vertical)
-                        .build();
+                        let box_scans_page = gtk::Box::builder()
+                            .orientation(gtk::Orientation::Vertical)
+                            .build();
 
-                    let box_scans_page_label = gtk::Label::new(Some("Box Scans"));
-                    box_scans_page.append(box_scan_image_viewer.widget());
-                    let page_number =
-                        root.append_page(&box_scans_page, Some(&box_scans_page_label));
-                    self.page_numbers.push(page_number);
-                    self.viewers.push(box_scan_image_viewer);
-                }
-                if let Some(file_set) = file_sets
-                    .iter()
-                    .find(|fs| fs.file_type == FileType::Screenshot.into())
-                {
-                    let image_viewer_init = ImageViewerInit {
-                        settings: Arc::clone(&self.settings),
-                        file_set: Some(file_set.clone()),
-                    };
-
-                    let box_scan_image_viewer =
-                        ImageViewer::builder().launch(image_viewer_init).detach();
-
-                    let box_scans_page = gtk::Box::builder()
-                        .orientation(gtk::Orientation::Vertical)
-                        .build();
-
-                    let box_scans_page_label = gtk::Label::new(Some("Screenshots"));
-                    box_scans_page.append(box_scan_image_viewer.widget());
-                    let page_number =
-                        root.append_page(&box_scans_page, Some(&box_scans_page_label));
-                    self.page_numbers.push(page_number);
-                    self.viewers.push(box_scan_image_viewer);
+                        let box_scans_page_label =
+                            gtk::Label::new(Some(file_type.to_string().as_str()));
+                        box_scans_page.append(box_scan_image_viewer.widget());
+                        let page_number =
+                            root.append_page(&box_scans_page, Some(&box_scans_page_label));
+                        self.page_numbers.push(page_number);
+                        self.viewers.push(box_scan_image_viewer);
+                    }
                 }
             }
             TabbedImageViewerMsg::Clear => {
                 for page_number in self.page_numbers.iter() {
                     root.remove_page(Some(*page_number));
                 }
+                self.page_numbers.clear();
             }
         }
     }
