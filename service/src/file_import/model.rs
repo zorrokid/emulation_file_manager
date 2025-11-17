dad5c8f8dfad32bd77d24e6d08f4ae9f2c7c9f22
Mikko Keinänen <mikko.keinanen@gmail.com>
2025-11-17 23:20:40 +0200

    cleanup

--- service/src/file_import/model.rs
+++ service/src/file_import/model.rs
@@ -14,18 +14,6 @@ pub struct ImportFileContent {
     pub existing_archive_file_name: Option<String>,
 }
 
-/// A file to be imported, containing its path and a mapping of its content by SHA1 checksum. When
-/// imported file is an archive, it may contain multiple files inside it. When it's a single file,
-/// the content will contain a single entry.
-/*#[derive(Debug)]
-pub struct ImportFile {
-    //pub file_set_name: String,      // TODO: is this needed?
-    //pub file_set_file_name: String, // TODO: is this needed?
-    //pub file_type: FileType,        // TODO: is this needed?
-    pub path: PathBuf,
-    pub content: HashMap<Sha1Checksum, ImportFileContent>,
-}*/
-
 #[derive(Debug)]
 pub struct FileImportModel {
     pub path: PathBuf,
@@ -35,8 +23,6 @@ pub struct FileImportModel {
 #[derive(Debug)]
 pub struct FileSetImportModel {
     pub import_files: Vec<FileImportModel>,
-    // TODO: maybe just remove other files than selected
-    // from import_files, so this can be removed?
     pub selected_files: Vec<Sha1Checksum>,
     pub system_ids: Vec<i64>,
     pub source: String,
